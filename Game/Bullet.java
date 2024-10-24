3ea14eada31eee0b233a1596a60057eb2b42ac67
HamstyTheHamster <149964114+HamstyTheHamster@users.noreply.github.com>
2024-10-24 11:58:06 +0200

    Update Bullet.java

--- Game/Bullet.java
+++ Game/Bullet.java
@@ -4,7 +4,7 @@ package Game;
 public class Bullet extends Entity{
     
     public Bullet(int startx, int starty, int tileSize) {
-        super(startx, starty, tileSize / 4, -10, "Bullet.png", 4);     //12x12
+        super(startx, starty, tileSize / 4, -3, "Bullet.png", 4);     //12x12
         //this.setPreferredSize(new Dimension(12, 12));  
     }
 }
