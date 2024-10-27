8ccbc63fb934e74dc136ae0b6a2d6d9ba7ae32ba
HamstyTheHamster <149964114+HamstyTheHamster@users.noreply.github.com>
2024-10-27 12:14:22 +0100

    added javadocs

--- Game/Run.java
+++ Game/Run.java
@@ -4,10 +4,22 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import javax.swing.JFrame;
 
+/**
+ * Class containing main method.
+ * takes no parameters
+ * displays frame for menu
+ */
 public class Run {
     JFrame window = new JFrame();
 
-
+    /**
+     * Constructor method to make the frame and put the elements in it.
+     * Takes no input.
+    * @throws FileNotFoundException
+    *      never happens
+    * @throws URISyntaxException
+    *      never happens
+    */
     public Run() throws FileNotFoundException, URISyntaxException{
         window.setTitle("COD ZOMBIES 2D");
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
